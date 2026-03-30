beae42fbcad8b3cebf775a64122c8408212d3890
htsteelp1 <htsteelp1@gmail.com>
2026-03-29 20:38:20 -0400

    Completed Exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let arrayString = string.split();
+    if (num>0) {
+        let finalString = "";
+        for (let i = 1; i<=num; i++)
+            finalString = arrayString.reduce((totalString, currentLetter) => {
+                return totalString + currentLetter;
+    },finalString);
+        return finalString;
+    }
+    else if (num === 0) {
+        return "";
+    }
+    else {
+        return "ERROR";
+    }
+
 
 };
 
