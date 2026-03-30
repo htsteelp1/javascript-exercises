d049f08c15aae59878c2a7028aee4c72c7faec6e
htsteelp1 <htsteelp1@gmail.com>
2026-03-29 21:21:12 -0400

    Completed Exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    if (!(parseInt(numA) === numA) || !(parseInt(numB) === numB) || numA < 0 || numB < 0) {
+        return "ERROR";
+    }
+    let smallNum = numA<numB ? numA : numB;
+    let bigNum = numB>numA ? numB : numA;
+    let sum = 0;
+    for (let i = smallNum; i<=bigNum; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
