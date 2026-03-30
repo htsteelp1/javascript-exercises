ba5a1dd38a1881db6aadb6ed82fd25c9b4457654
htsteelp1 <htsteelp1@gmail.com>
2026-03-29 21:13:26 -0400

    Completed Exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = 1; i < arguments.length; i++) {
+        array = array.filter((value) => !(value===arguments[i]))
+    }
+    return array;
+
+
 };
 
 // Do not edit below this line
