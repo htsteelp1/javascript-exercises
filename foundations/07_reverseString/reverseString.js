181249480694fb2877aaf59bed57e57e2243df0f
htsteelp1 <htsteelp1@gmail.com>
2026-03-29 20:51:39 -0400

    Completed Exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayString = string.split('');
+    let reverseArray = [];
+    for (let i = 0; i<string.length; i++) {
+        reverseArray.push(arrayString.pop());
+    }
+    let reverseString = reverseArray.join('')
+    return reverseString;
 };
 
 // Do not edit below this line
